3f3fc9ddf7d8dd1d5617f66f240e40db5f76e5e0
Florian Spiess <florian.spiess@unibas.ch>
2024-10-28 13:26:05 +0100

    Implement function to list evaluations

--- Sources/DresSwiftClient/DresSwiftClient.swift
+++ Sources/DresSwiftClient/DresSwiftClient.swift
@@ -6,6 +6,7 @@ public class DresClient {
     let client: Client
     let username, session: String
     
+    /// Instantiate client and perform login.
     public init(url: URL, username: String, password: String) async throws {
         self.client = Client(serverURL: url, transport: URLSessionTransport())
         
@@ -15,5 +16,16 @@ public class DresClient {
         
         self.username = message.username!
         self.session = message.sessionId!
+    }
+    
+    /// Retrieves and lists the evaluations currently available to this client.
+    ///
+    /// - Returns: Array of tuples containing the name and ID of each evaluation.
+    public func listEvaluations() async throws -> [(name: String, id: String)] {
+        let response = try await client.getApiV2ClientEvaluationList(.init(query: .init(session: session)))
+        
+        let message = try response.ok.body.json
+        
+        return message.map { ($0.name, $0.id) }
     }
 }
--- Tests/DresSwiftClientTests/DresSwiftClientTests.swift
+++ Tests/DresSwiftClientTests/DresSwiftClientTests.swift
@@ -5,7 +5,11 @@ import Foundation
 let username = "REPLACE_WITH_YOUR_USERNAME"
 let password = "REPLACE_WITH_YOUR_PASSWORD"
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+@Test func login() async throws {
     _ = try await DresClient(url: URL(string: "https://vbs.videobrowsing.org")!, username: username, password: password)
+}
+
+@Test func listEvaluations() async throws {
+    let client = try await DresClient(url: URL(string: "https://vbs.videobrowsing.org")!, username: username, password: password)
+    _ = try await client.listEvaluations()
 }
